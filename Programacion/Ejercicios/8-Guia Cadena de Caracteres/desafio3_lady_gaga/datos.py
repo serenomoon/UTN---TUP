42a866f66eb2e1f93d1cb60ec0dba181997f3353
serenomoon <saulofernandezibar@gmail.com>
2025-06-09 15:02:46 -0300

    terminado gaga

--- Programacion/Ejercicios/8-Guia Cadena de Caracteres/desafio3_lady_gaga/datos.py
+++ Programacion/Ejercicios/8-Guia Cadena de Caracteres/desafio3_lady_gaga/datos.py
@@ -1,8 +1,11 @@
 # 🎵 Análisis de Lista de Reproducción - Lady Gaga
 # Disponemos de información sobre una lista de reproducción de la cantante Lady Gaga, y nuestro objetivo es desarrollar funciones para procesar y normalizar estos datos para un posterior análisis.
 # 📌 Datos de Ejemplo
-tema = "Bradley Cooper | Lukas Nelson - Shallow"
-vistas = "1900 millones"
-duración = "3:37"
-link = "https://www.youtube.com/watch?v=bo_efYhYU2A"
-fecha_de_lanzamiento = "2018-09-27"
+
+gaga = {
+    "tema": "Bradley Cooper | Lukas Nelson - Shallow",
+    "vistas": "1900 millones",
+    "duracion": "3:37",
+    "link": "https://www.youtube.com/watch?v=bo_efYhYU2A",
+    "fecha_de_lanzamiento": "2018-09-27",
+}
--- Programacion/Ejercicios/8-Guia Cadena de Caracteres/desafio3_lady_gaga/funciones.py
+++ Programacion/Ejercicios/8-Guia Cadena de Caracteres/desafio3_lady_gaga/funciones.py
@@ -1,44 +1,115 @@
-from datetime import date
+from datetime import datetime
 from datos import *
 
+def split_string(cadena: str, caracter: str = " ", numero: int = 2) -> list:
+    """Separa un array en dos partes y devuelve una de las dos o ambos
+
+    Args:
+        cadena (str): string a dividir
+        caracter (str, optional): Caracter donde se efectua la division. Defaults to " ".
+        numero (int, optional): Parte del string que se devuelve. Defaults to 2.
+
+    Returns:
+        list: _description_
+    """
+    spliteado = cadena.split(caracter)
+    if numero == 0:
+        spliteado = spliteado[0]
+    elif numero == 1:
+        spliteado = spliteado[1]
+    return spliteado
+
+
 def obtener_colaboradores(titulo: str) -> list:
-    colaboradores_recortados = titulo.split("-")[0]
+    """Recibe string de tema y colaboradores, devuelve colaboradores en un array
+
+    Args:
+        titulo (str): Nombre del tema y sus colaboradores
+
+    Returns:
+        list: Lista de colaboradores
+    """
+    colaboradores_recortados = split_string(titulo,"-",0)
     nueva_lista = []
     for i in range(len(colaboradores_recortados.split("|"))):
         nuevo = colaboradores_recortados.split("|")[i].strip()
         nueva_lista.append(nuevo)
     return nueva_lista
-    # Recibe como parámetro el título del tema.
-    # Retorna una lista con los colaboradores (excluyendo el nombre del tema).
+
 
 def obtener_nombre_tema(titulo: str) -> str:
-    return titulo.split("-")[1].strip()
-    # Recibe el título del tema.
-    # Retorna el nombre real del tema, eliminando a los colaboradores.
+    """Recibe el título del tema y colaboradores.
+    Retorna solo el nombre del tema
+
+    Args:
+        titulo (str): tema y colaboradores
+
+    Returns:
+        str: nombre del tema
+    """
+    return split_string(titulo,"-",1).strip()
 
 def convertir_vistas_numerico(vistas: str) -> int:
-    numeros = int(vistas.split(" ")[0])
-    letras = vistas.split(" ")[1]
+    """Convierte un numero en numeros y letras a solo numeros.
+    Ejemplo: "1900 millones" → 1900000000.
+
+    Args:
+        vistas (str): numero y letras
+
+    Returns:
+        int: numero entero
+    """
+    numero_dividido = split_string(vistas)
     
-    if letras == "millones":
-        numeros = numeros*1_000_000
-    elif letras == "mil":
-        numeros = numeros*1_000
-    return numeros
-    # Convierte la cantidad de vistas a un número entero expresado en millones.
-    # Ejemplo: "1900 millones" → 1900000000.
+    if numero_dividido[1] == "millones":
+        numero = int(numero_dividido[0])*1_000_000
+    elif numero_dividido[0] == "mil":
+        numero = int(numero_dividido[0])*1_000
+    return numero
+
 
 def convertir_duracion_numerico(duracion: str) -> int:
-    pass
-    # Convierte la duración del video a un número entero expresado en segundos.
-    # Ejemplo: "3:37" → 217 segundos.
+    """Convierte formato min y seg a un número entero expresado en segundos.
+        Ejemplo: "3:37" → 217 segundos
+
+    Args:
+        duracion (str): minutos y segundo ingresados
+
+    Returns:
+        int: segundos
+    """
+    segundos_divididos = split_string(duracion,":")
+    segundos = int(segundos_divididos[0])*60 + int(segundos_divididos[1])
+    return segundos
+
 
 def obtener_codigo(url: str) -> str:
-    pass
-    # Recibe una URL de YouTube.
-    # Extrae y retorna el código único del video.
-    # Ejemplo: "https://www.youtube.com/watch?v=bo_efYhYU2A" → "bo_efYhYU2A".
+    """Recibe una URL de YouTube.
+        Extrae y retorna el código único del video.
+        Ejemplo: "https://www.youtube.com/watch?v=bo_efYhYU2A" → "bo_efYhYU2A".
 
-def formatear_fecha(fecha: str) -> date:
-    pass
-    # Recibe una fecha en formato YYYY-MM-DD.
+    Args:
+        url (str): link del video
+
+    Returns:
+        str: Codigo del video
+    """
+    codigo = split_string(url,"?v=",1)
+    return codigo
+
+
+def formatear_fecha(fecha: str) -> datetime:
+    """Recibe una fecha en formato YYYY-MM-DD.
+    Retorna la fecha como un objeto de tipo date.
+
+    Args:
+        fecha (str): fecha string
+
+    Returns:
+        datetime: fecha tipo date
+    """
+    fecha_convertida = datetime.strptime(fecha, "%Y-%m-%d").date()
+    return fecha_convertida
+
+fecha = formatear_fecha(gaga["fecha_de_lanzamiento"])
+print(type(fecha))
